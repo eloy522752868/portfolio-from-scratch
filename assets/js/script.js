323bbae5c811b40cac4a1d33b13152fb00495932
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-06 09:33:50 -0400

    added click event for resume with js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 var docLocation = "./assets/images/Eloy Gonzalez 2020 Resume.pdf";
-window.open(docLocation,"resizeable,scrollbar"); 
+
+//Add class to resume nav link when clicked
+$('#resume-detail').on("click", function(e){ 
+ 
+ window.open(docLocation,"resizeable,scrollbar"); 
+ e.preventDefault(); 
+
+});
+
