afd71d40384052b962efefcc23879acfe2f8cfea
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-06 16:30:21 -0400

    add js and function clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,65 @@
 var docLocation = "./assets/images/Eloy Gonzalez 2020 Resume.pdf";
+var linkIn = "https://www.linkedin.com/in/egonzalez1/";
+var gitHubRepos = ["https://github.com/mknowlton89/trademark_search","https://github.com/eloy522752868/interview-coding-assessment","https://github.com/eloy522752868/employees-passwords-generator","https://github.com/eloy522752868/work-day-scheduler"];
 
+
+console.log(gitHubRepos);
 //Add class to resume nav link when clicked egonSadd 
-$('#resume-detail').on("click", function(e){ 
- 
- window.open(docLocation,"resizeable,scrollbar"); 
- e.preventDefault(); 
+$('#resume-detail').on("click", function(){ 
+    resumeGenerator();
 
 });
 
+//Add pro
+
+$('#img-profile').on("click", function(){ 
+    linkInsSite();
+});
+
+$('#trademark-project').on("click", function(){ 
+    gitHubReposTradeSearch();
+});
+
+$('#coding-quiz-challengeid').on("click", function(){ 
+    gitHubReposCodingAssessment();
+});
+
+$('#employees-passwords-generatorid').on("click", function(){ 
+    gitHubReposPasswordsGenerator();
+});
+
+$('#WorkdaySchedulerid').on("click", function(){ 
+    gitHubReposWorkDayScheduler();
+});
+
+
+function linkInsSite()
+ {
+    window.open(linkIn,"resizeable,_none"); 
+ }
+
+
+ function resumeGenerator()
+ {
+    window.open(docLocation,"resizeable,scrollbar"); 
+ }
+
+ function gitHubReposTradeSearch()
+ {
+    window.open(gitHubRepos[0],"_none"); 
+ }
+
+ function gitHubReposCodingAssessment()
+ {
+    window.open(gitHubRepos[1],"_none"); 
+ }
+
+ function gitHubReposPasswordsGenerator()
+ {
+    window.open(gitHubRepos[2],"_none"); 
+ }
+
+ function gitHubReposWorkDayScheduler()
+ {
+    window.open(gitHubRepos[3],"_none"); 
+ }
