04af1d54bf71a307d6385fede2ef26fcf171d933
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-06 10:00:22 -0400

    adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var docLocation = "./assets/images/Eloy Gonzalez 2020 Resume.pdf";
 
-//Add class to resume nav link when clicked
+//Add class to resume nav link when clicked egonSadd 
 $('#resume-detail').on("click", function(e){ 
  
  window.open(docLocation,"resizeable,scrollbar"); 
