2c67cee87fdb4c7e4069dd44b24242c72cf69cea
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-06 16:54:18 -0400

    link changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var docLocation = "./assets/images/Eloy Gonzalez 2020 Resume.pdf";
 var linkIn = "https://www.linkedin.com/in/egonzalez1/";
-var gitHubRepos = ["https://github.com/mknowlton89/trademark_search","https://github.com/eloy522752868/interview-coding-assessment","https://github.com/eloy522752868/employees-passwords-generator","https://github.com/eloy522752868/work-day-scheduler"];
+var gitHubRepos = ["https://github.com/mknowlton89/trademark_search","https://github.com/eloy522752868/interview-coding-assessment","https://github.com/eloy522752868/employees-passwords-generator","https://github.com/eloy522752868/work-day-scheduler","https://github.com/eloy522752868?tab=repositories"];
 
 
 console.log(gitHubRepos);
@@ -11,6 +11,17 @@ $('#resume-detail').on("click", function(){
 });
 
 //Add pro
+
+
+$('#linked-in').on("click", function()
+{ 
+    linkInsSite();
+});
+
+$('#git-hub').on("click", function()
+{ 
+    gitHubReposGeneralRepo();
+});
 
 $('#img-profile').on("click", function(){ 
     linkInsSite();
@@ -62,4 +73,10 @@ function linkInsSite()
  function gitHubReposWorkDayScheduler()
  {
     window.open(gitHubRepos[3],"_none"); 
+ }
+
+ 
+ function gitHubReposGeneralRepo()
+ {
+    window.open(gitHubRepos[4],"_none"); 
  }
